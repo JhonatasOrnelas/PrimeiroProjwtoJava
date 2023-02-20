b1d59e4cc5c5b5fe161a0e167b37805be6996cec
admin <admin@SergioOrnelasCardoso>
2023-02-20 13:36:25 -0200

    Criação do projeto

---
+++ primeiro-programa-Java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/* Main é um metodo auto executavel em Java */
+	public static void main(String[] args) {
+
+		/* Escreva texto no console */
+		System.out.println("meu primeiro programa em java - curso");
+		System.out.println("Java e orientado a objetos");
+	}
+}

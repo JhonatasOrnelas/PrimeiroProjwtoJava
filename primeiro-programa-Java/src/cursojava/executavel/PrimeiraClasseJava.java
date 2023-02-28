8172e29a72718c6ab478072c98116f863d7e99c3
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-02-28 09:44:39 -0300

    Revert "adicionando data de nascimento2"
    This reverts commit f3d907578d9752fb73583adc451e25428830aa72.

--- primeiro-programa-Java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-Java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -11,8 +11,6 @@ public class PrimeiraClasseJava {
 	private int idade;
 	
 	private Date dataNascimento;
-	
-	private Date dataNascimento2;	
 
 	public Date getDataNascimento() {
 		return dataNascimento;

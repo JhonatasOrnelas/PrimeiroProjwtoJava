092f63dac1d105bcffc51e93a54d83f9a05b4aa0
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-09 09:20:40 -0300

    media do aluno em java

--- primeiro-programa-Java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-Java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,13 +6,6 @@ public class Primeiro_Programa_Java {
 		/*Main � um metodo alto executavel em Java*/
 		public static void main(String[] args) {
 		
-		/*Concatenação = Unir ou Juntar dados*/	
-		String nome = "jhonatas";
-		String cpf = "123-234-345-45";
-		String telefone = "12 1234-5656";
-		String endereço = "guarulhos";
-		
-	 	System.out.println("meu nome é = " + nome + " " + "e meu cpf é = " + cpf + " meu contato é = " + telefone + " e moro em = " + endereço);
 		
 	 	
 		}

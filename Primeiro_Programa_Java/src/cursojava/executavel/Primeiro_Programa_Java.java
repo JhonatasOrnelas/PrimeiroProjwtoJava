99ab350b5be7c3ebdc77339717e474f128278192
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-09 09:31:56 -0300

    Concatenação = Unir ou Juntar dados em java

---
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -0,0 +1,26 @@
+package cursojava.executavel;
+
+public class Primeiro_Programa_Java {
+	
+	
+	/*Main � um metodo alto executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Concatenação = Unir ou Juntar dados*/
+		
+		String textoQualquer = "jhonatas ornelas cardoso";
+		
+		String nome = "jhonatas";
+		String cpf = "123-234-345-45";
+		String telefone = "12 1234-5656";
+		String endereço = "guarulhos";
+		
+	 	System.out.println("meu nome é = " + nome + " " + "e meu cpf é = " + cpf + " meu contato é = " + telefone + " e moro em = " + endereço);
+		
+		
+		
+		}
+		
+	
+	
+}

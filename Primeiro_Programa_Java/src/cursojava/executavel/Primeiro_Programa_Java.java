2374c50fa15e8e18e9063d68925cc6a0928a16b2
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-10 09:14:22 -0300

    teste da aula 8.1

--- Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -1,18 +1,22 @@
-package cursojava.executavel;
 
-public class Primeiro_Programa_Java {
-	
-	
-	/*Main � um metodo alto executavel em Java*/
+public class teste {
+
+	/* Main � um metodo alto executavel em Java */
 	public static void main(String[] args) {
-		
 
-	
+		int mediaAluno = 50;
+		String nome = "jhonatas";
+
+		if (mediaAluno >= 50 && nome.equals("jhonatas")) {
+			System.out.println("aluno aprovado");
+		} else if (mediaAluno < 50 && nome.equals("jhonatas")) {
+			System.out.println("aluno reprovado");
+		}else {
+			System.out.println("aluno desconhecido");
+				
+		}
+
 	}
 
-	
-	}
-		
-	
-	
 }
+

ea5cf42a61470f3b33ef8e3766846c5ec5d0642b
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-12 09:42:08 -0300

    Operadores ternareos aula 8.2

--- Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -5,14 +5,16 @@ public class teste {
 
 	int nota1 = 90; 
 	int nota2 = 80; 
-	int nota3 = 70; 
-	int nota4 = 60; 
+	int nota3 = 80; 
+	int nota4 = 90; 
 	int media = 0;
 	
 	media = (nota1 + nota2 + nota3 + nota4) /4;
 	
+	/*Condiações com IF e ELSE*/
+	
 	if (media >= 70) {
-		System.out.println("Aluno aprovado : " + media);	
+		System.out.println("Aluno aprovado : " + media);
 	}
 	if (media >= 40 && media <= 69) {
 		System.out.println("Aluno em recuperação : " + media);
@@ -21,6 +23,13 @@ public class teste {
 		System.out.println("Aluno reprovado : " + media);
 		
 	}
+	
+	/*Operadores ternareos sao para micro validações*/
+	
+	String saidaResultado;
+	
+	saidaResultado = media >= 70 ? "aluno aprovado" : (media >= 40 && media <= 69) ? "aluno em recuperação" : "reprovado";
+			System.out.println(saidaResultado);
 	
 	}
 

fb4f620bcc3e856bea4b9ba6a68296d41eaea363
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-12 10:34:04 -0300

    Switch Case: operações exatas aula 8.4

--- Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -3,31 +3,39 @@ public class teste {
 	/* Main � um metodo alto executavel em Java */
 	public static void main(String[] args) {
 
-	int nota1 = 90; 
-	int nota2 = 90; 
-	int nota3 = 90; 
-	int nota4 = 90; 
+	int nota1 = 70; 
+	int nota2 = 70; 
+	int nota3 = 70; 
+	int nota4 = 70; 
 	int media = 0;
 	
 	media = (nota1 + nota2 + nota3 + nota4) /4;
 	
-	/*Operações logicas aninhadas: Sao perações dentro de operações*/
-	
-	if (media >= 50) {
-	if (media >= 70) {
-	if (media >= 90) {
-		System.out.println("Aluno aprovado = " + media + " pontos" + " - exelente");
-	}else {
-		System.out.println( "Aluno aprovado = " + media + " pontos");
-	}
-	
-	}else {
-		System.out.println( "Aluno em recuperação = " + media + " pontos");
-  }
+	/*Switch Case: operações exatas*/
+	int dia = 6;
+	switch (dia) {
+	case 1: 
+		System.out.println("domingo");
+		break;
+	case 2:
+		System.out.println("Segunda feira");
+		break;
+	case 3:
+		System.out.println("terça feira");
+		break;
+	case 4:
+		System.out.println("Quarta feira");
+		break;
+	case 5:
+		System.out.println("Quinta feira");
+		break;
+	case 6:
+		System.out.println("Sexta feira");
+		break;	
 		
-    }else {
-    	System.out.println( "Aluno reprovado = " + media + " pontos");
-    }
+	default: System.out.println("Outro dia qualquer");
+			
+	}
 	
   }
 

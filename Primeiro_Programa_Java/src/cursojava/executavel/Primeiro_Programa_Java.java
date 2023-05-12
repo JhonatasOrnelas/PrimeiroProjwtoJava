e6b4bb8119bcc6d07332d3a15ececb8b37ae6d85
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-12 10:11:47 -0300

    Operações logicas aninhadas aula 8.3

--- Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -4,33 +4,31 @@ public class teste {
 	public static void main(String[] args) {
 
 	int nota1 = 90; 
-	int nota2 = 80; 
-	int nota3 = 80; 
+	int nota2 = 90; 
+	int nota3 = 90; 
 	int nota4 = 90; 
 	int media = 0;
 	
 	media = (nota1 + nota2 + nota3 + nota4) /4;
 	
-	/*Condiações com IF e ELSE*/
+	/*Operações logicas aninhadas: Sao perações dentro de operações*/
 	
+	if (media >= 50) {
 	if (media >= 70) {
-		System.out.println("Aluno aprovado : " + media);
+	if (media >= 90) {
+		System.out.println("Aluno aprovado = " + media + " pontos" + " - exelente");
+	}else {
+		System.out.println( "Aluno aprovado = " + media + " pontos");
 	}
-	if (media >= 40 && media <= 69) {
-		System.out.println("Aluno em recuperação : " + media);
-	}
-	else {
-		System.out.println("Aluno reprovado : " + media);
+	
+	}else {
+		System.out.println( "Aluno em recuperação = " + media + " pontos");
+  }
 		
-	}
+    }else {
+    	System.out.println( "Aluno reprovado = " + media + " pontos");
+    }
 	
-	/*Operadores ternareos sao para micro validações*/
-	
-	String saidaResultado;
-	
-	saidaResultado = media >= 70 ? "aluno aprovado" : (media >= 40 && media <= 69) ? "aluno em recuperação" : "reprovado";
-			System.out.println(saidaResultado);
-	
-	}
+  }
 
 }

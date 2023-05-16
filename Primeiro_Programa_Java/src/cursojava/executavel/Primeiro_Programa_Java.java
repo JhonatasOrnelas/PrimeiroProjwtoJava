cad451dd15643286d049c6bc1789f573906a4789
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-16 14:13:53 -0300

    Ajuste Java

--- Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -5,16 +5,21 @@ public class teste {
 
 		/* Estrutura de repetição FOR */
 
-	for (int numero = 0; numero <= 10; numero ++) {
-	     if (numero == 5) {
-	        System.out.println("numero 5 encontrado");	
-	        System.out.println("parando exucução");
-	        break;
-    }
-	
-	
-    }
+		for (int numero = 0; numero <= 10; numero++) {
+			if (numero == 5) {
+				System.out.println("numero 5 encontrado");
+				System.out.println("parando exucução");
+				break;
+			}
+			/*------------------------------------------*/
+			/*
+			 * for (int numero1 = 10; numero1 >= 0; numero--) { if (numero1 == 8) {
+			 * System.out.println("numero1 5 encontrado");
+			 * System.out.println("parando exucução"); break; }
+			 */
+
+		}
 
 	}
-	
+
 }

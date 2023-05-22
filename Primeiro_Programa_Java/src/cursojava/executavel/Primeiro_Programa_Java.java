2233deee8f13268710cf021c1a48940608a71dad
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-22 08:58:58 -0300

    aula 8.13

--- Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -4,37 +4,33 @@ public class PrimeiroProjetoJava {
 
 	/* Main � um metodo alto executavel em Java */
 	public static void main(String[] args) {
-		
-	/*Modulo : resto da divisão % */
-		
-		
-	String carros = JOptionPane.showInputDialog("informe a quantidade de carros");
-	String pessoas = JOptionPane.showInputDialog("informe a quantidade de pessoas");
-	
-	double carroNumero = Double.parseDouble(carros);
-	double pessoaNumero = Double.parseDouble(pessoas);
-	
-	int divisao = (int) (carroNumero / pessoaNumero);
-	
-	double resto = carroNumero % pessoaNumero;
-	
-	int resposta = JOptionPane.showConfirmDialog(null, "deseja ver o resultado da divisao?")
-			
-	if (resposta == 0) {
-		JOptionPane.showMessageDialog(null, "divisao para as pessoas deu " + divisao )
-		
-	}else {
-		System.out.println("nao quiz ver o resultado");
-	} 	
 
-	resposta = JOptionPane.showMessageDialog(null, "deseja ver o resultado da divisao?")
-			
-	if (resposta == 0) {
-		JOptionPane.showMessageDialog(null, "o resto da divisao é " + resto )
-		
-	}
-	
-	
+		String nota = JOptionPane.showInputDialog("informe a nota 1");
+		String nota = JOptionPane.showInputDialog("informe a nota 2");
+		String nota = JOptionPane.showInputDialog("informe a nota 3");
+		String nota = JOptionPane.showInputDialog("informe a nota 4");
+
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+
+		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+
+		/* Media para aprovação é 70 */
+
+		if (media >= 50) {
+
+			if (media >= 70) {
+				JOptionPane.showMessageDialog(null, "aluno esta aprovado com a nota :" + media);
+			} else {
+				JOptionPane.showMessageDialog(null, "aluno em recuperação com a media:" + media);
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(null, "A media é :" + media);
+		}
+
 	}
 
 }

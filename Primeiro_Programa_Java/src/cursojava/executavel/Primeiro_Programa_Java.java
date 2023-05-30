b1935dbc734514d960126333e38ca371e136f646
JhonatasOrnelas <jho.ornelas.cardoso@gmail.com>
2023-05-30 09:33:29 -0300

    aula 9.6

--- Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
+++ Primeiro_Programa_Java/src/cursojava/executavel/Primeiro_Programa_Java.java
@@ -1,36 +1,16 @@
-import javax.swing.JOptionPane;
+package cursoJava.classes;
 
-public class PrimeiroProjetoJava {
+/*Este é a nova classe/objeto que representa o aluno*/
+public class Aluno {
 
-	/* Main � um metodo alto executavel em Java */
-	public static void main(String[] args) {
-
-		String nota = JOptionPane.showInputDialog("informe a nota 1");
-		String nota = JOptionPane.showInputDialog("informe a nota 2");
-		String nota = JOptionPane.showInputDialog("informe a nota 3");
-		String nota = JOptionPane.showInputDialog("informe a nota 4");
-
-		double dNota1 = Double.parseDouble(nota1);
-		double dNota2 = Double.parseDouble(nota2);
-		double dNota3 = Double.parseDouble(nota3);
-		double dNota4 = Double.parseDouble(nota4);
-
-		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
-
-		/* Media para aprovação é 70 */
-
-		if (media >= 50) {
-
-			if (media >= 70) {
-				JOptionPane.showMessageDialog(null, "aluno esta aprovado com a nota :" + media);
-			} else {
-				JOptionPane.showMessageDialog(null, "aluno em recuperação com a media:" + media);
-			}
-
-		} else {
-			JOptionPane.showMessageDialog(null, "A media é :" + media);
-		}
-
-	}
-
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatricola;
 }
